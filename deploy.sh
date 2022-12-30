210e946bdbfa057427862f883f81469b62864a31
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-12-29 19:46:47 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,11 @@
 # Check container service status
 # aws lightsail get-container-services
 
-# push container image to service
+# [push container image to service](https://lightsail.aws.amazon.com/ls/docs/en_us/articles/amazon-lightsail-pushing-container-images)
+# aws lightsail push-container-image --region us-west-2 --service-name myservice --label mystaticwebsite --image mystaticwebsite:v2
 # aws lightsail push-container-image --service-name flask-service --label flask-container --image flask-container
+
+
 
 # Deploy the container to the container service with the AWS CLI
 # aws lightsail create-container-service-deployment --service-name flask-service --containers file://containers.json --public-endpoint file://public-
